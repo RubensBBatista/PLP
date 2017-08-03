dae9a1b7957ebbbc7fc10772354fd6b3a471b655
RubensBBatista <rubens.sousa@ccc.ufcg.edu.br>
2017-08-03 17:22:09 -0300

    vogais

--- prolog_another/vogais.pl
+++ prolog_another/vogais.pl
@@ -1,3 +1,12 @@
-isVogal(letra) :-
-    member(letra,["a","e","i","o","u"]).
+isVogal(A,S) :-
+    member(A,["a","e","i","o","u"]) -> S = A
 
+getElement(I,[H|T],C) :- I == 0 -> C = isVogal(H,S); Z is I - 1, getElement(Z,T,C).
+
+
+
+main :-
+  read_line_to_codes(user_input, A2),
+  string_to_atom(A2,A1),
+  atom_number(A1,A),
+  
