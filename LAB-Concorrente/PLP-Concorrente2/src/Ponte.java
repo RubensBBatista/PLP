f5c4d4708b1c7f8a22303844a540885817e00adc
RubensBBatista <rubens.sousa@ccc.ufcg.edu.br>
2017-08-25 15:41:18 -0300

    Questao2

---
+++ LAB-Concorrente/PLP-Concorrente2/src/Ponte.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ponte implements Runnable {
+
+	private List<String> fila;
+
+	public Ponte() {
+		this.fila = new ArrayList<>();
+	}
+
+	public void carro(String carro) {
+		fila.add(carro);
+	}
+
+	@Override
+	public void run() {
+		while (!this.fila.isEmpty()) {
+			if (this.fila.contains("A")) {
+				System.out.println("Passando carro A --> B");
+				this.fila.remove("A");
+			} else if (this.fila.contains("B")) {
+				System.out.println("Passando carro B --> A");
+				this.fila.remove("B");
+			}
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				System.out.println(e.getMessage());
+			}
+		}
+	}
+
+}
---
+++ LAB-Concorrente/PLP-Concorrente2/src/QuestaoDois.java
@@ -0,0 +1,13 @@
+
+public class QuestaoDois {
+
+	public	static	void main(String[] args)	{
+		Ponte p	= new Ponte();
+		Thread t = new Thread(p);
+		t.start();
+		p.carro("A");
+		p.carro("B");
+		p.carro("A");
+	}
+
+}
