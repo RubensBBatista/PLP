c6068715874473eb34840b150142396efb6b9e16
Rubens Batista <rubens.sousa@ccc.ufcg.edu.br>
2017-08-25 17:25:57 -0300

    questo 2 mudada

--- LAB-Concorrente/PLP-Concorrente2/src/Ponte.java
+++ LAB-Concorrente/PLP-Concorrente2/src/Ponte.java
@@ -4,29 +4,46 @@ import java.util.List;
 public class Ponte implements Runnable {
 
 	private List<String> fila;
-
+	
 	public Ponte() {
 		this.fila = new ArrayList<>();
 	}
-
 	public void carro(String carro) {
 		fila.add(carro);
 	}
 
 	@Override
 	public void run() {
-		while (!this.fila.isEmpty()) {
-			if (this.fila.contains("A")) {
-				System.out.println("Passando carro A --> B");
-				this.fila.remove("A");
-			} else if (this.fila.contains("B")) {
-				System.out.println("Passando carro B --> A");
-				this.fila.remove("B");
-			}
+		String primeiro = this.fila.get(0);
+		for (int i = 0; i < fila.size(); i++) {
+			if (this.fila.contains(primeiro)) {
+				if(primeiro.equals("A"))
+					System.out.println("Passando carro A --> B");
+				else
+					System.out.println("Passando carro B --> A");
+				this.fila.remove(primeiro);
+			} 
 			try {
 				Thread.sleep(2000);
 			} catch (InterruptedException e) {
 				System.out.println(e.getMessage());
+			}
+		}
+		if(!this.fila.isEmpty()) {
+			primeiro = this.fila.get(0);
+			while(!this.fila.isEmpty()) {
+				if (this.fila.contains(primeiro)) {
+					if(primeiro.equals("A"))
+						System.out.println("Passando carro A --> B");
+					else
+						System.out.println("Passando carro B --> A");
+					this.fila.remove(primeiro);
+				}
+				try {
+					Thread.sleep(2000);
+				} catch (InterruptedException e) {
+					System.out.println(e.getMessage());
+				}
 			}
 		}
 	}
